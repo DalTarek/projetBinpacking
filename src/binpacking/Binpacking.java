74f83ef74f445bd53f310895e2461dc268282033
Mohammade Dalati <mohammade.dalati7@etu.univ-lorraine.fr>
2018-01-07 03:38:48 +0100

    Modif question 16

--- src/binpacking/Binpacking.java
+++ src/binpacking/Binpacking.java
@@ -1,5 +1,8 @@
 package binpacking;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Random;
@@ -173,8 +176,8 @@ public class Binpacking {
 	}
 	
 	//Test Q16 
-	public static int test(){
-		int n, hboites, valeur, taille, fp, ff, bf, ffd, bfd;
+	public static void test(){
+		int n, valeur, taille, fp, ff, bf, ffd, bfd;
 		ArrayList<Integer> tabfp = new ArrayList<>();
 		ArrayList<Integer> tabff = new ArrayList<>();
 		ArrayList<Integer> tabbf = new ArrayList<>();
@@ -182,35 +185,105 @@ public class Binpacking {
 		ArrayList<Integer> tabbfd = new ArrayList<>();
 		Random r;
 		ArrayList<Integer> objets;
-		for(int i = 1; i < 11; i++){
-			fp = 0; 
-			ff = 0;
-			bf = 0; 
-			ffd = 0; 
-			bfd = 0;
-			n = i * 100;
-			hboites = (int) (1.5 * n);
-			taille = (int) (n * 1.5);
-			for(int j = 0; j < 20; j++){
-				objets = new ArrayList<>();
-				for(int o = 0; o < n; o++){
-					r = new Random();
-					valeur = (int) (0.2 * n + r.nextInt((int)(0.8 * n - 0.2 * n)));
-					objets.add(valeur);
+		try {
+			BufferedWriter bw1 = new BufferedWriter(new FileWriter("data_fractional_packing.dat"));
+			BufferedWriter bw2 = new BufferedWriter(new FileWriter("data_first_fit_packing.dat"));
+			BufferedWriter bw3 = new BufferedWriter(new FileWriter("data_best_fit_packing.dat"));
+			BufferedWriter bw4 = new BufferedWriter(new FileWriter("data_first_fit_decreasing_packing.dat"));
+			BufferedWriter bw5 = new BufferedWriter(new FileWriter("data_best_fit_decreasing_packing.dat"));
+			
+			bw1.write("0");
+			bw1.write(" 0");
+			bw1.newLine();
+			
+			bw2.write("0");
+			bw2.write(" 0");
+			bw2.newLine();
+			
+			bw3.write("0");
+			bw3.write(" 0");
+			bw3.newLine();
+			
+			bw4.write("0");
+			bw4.write(" 0");
+			bw4.newLine();
+			
+			bw5.write("0");
+			bw5.write(" 0");
+			bw5.newLine();
+			
+			for(int i = 1; i < 11; i++){
+				fp = 0; 
+				ff = 0;
+				bf = 0; 
+				ffd = 0; 
+				bfd = 0;
+				n = i * 100;
+				taille = (int) (n * 1.5);
+				for(int j = 0; j < 20; j++){
+					objets = new ArrayList<>();
+					for(int o = 0; o < n; o++){
+						r = new Random();
+						valeur = (int) (0.2 * n + r.nextInt((int)(0.8 * n - 0.2 * n)));
+						objets.add(valeur);
+					}
+					fp = fp + fractionalPacking(objets,taille);
+					ff = ff + getNbUtilisées(firstFitPacking(objets,taille));
+					bf = bf + getNbUtilisées(bestFitPacking(objets,taille));
+					ffd = ffd + getNbUtilisées(firstFitDecreasingPacking(objets, taille));
+					bfd = bfd + getNbUtilisées(bestFitDecreasingPacking(objets, taille));
 				}
-				fp = fp + fractionalPacking(objets,taille);
-				ff = ff + getNbUtilisées(firstFitPacking(objets,taille));
-				bf = bf + getNbUtilisées(bestFitPacking(objets,taille));
-				ffd = ffd + getNbUtilisées(firstFitDecreasingPacking(objets, taille));
-				bfd = bfd + getNbUtilisées(bestFitDecreasingPacking(objets, taille));
+				tabfp.add(fp / 20); 
+				tabff.add(ff / 20); 
+				tabbf.add(bf / 20); 
+				tabffd.add(ffd / 20); 
+				tabbfd.add(bfd / 20);
+				
+				bw1.write("" + n);
+				bw1.write(" " + tabfp.get(i-1));
+				bw1.newLine();
+				
+				bw2.write("" + n);
+				bw2.write(" " + tabff.get(i-1));
+				bw2.newLine();
+				
+				bw3.write("" + n);
+				bw3.write(" " + tabbf.get(i-1));
+				bw3.newLine();
+				
+				bw4.write("" + n);
+				bw4.write(" " + tabffd.get(i-1));
+				bw4.newLine();
+				
+				bw5.write("" + n);
+				bw5.write(" " + tabbfd.get(i-1));
+				bw5.newLine();
 			}
-			tabfp.add(fp / 20); 
-			tabff.add(ff / 20); 
-			tabbf.add(bf / 20); 
-			tabffd.add(ffd / 20); 
-			tabbfd.add(bfd / 20); 
+			
+			bw1.write("1100");
+			bw1.write(" 0");
+			
+			bw2.write("1100");
+			bw2.write(" 0");
+			
+			bw3.write("1100");
+			bw3.write(" 0");
+			
+			bw4.write("1100");
+			bw4.write(" 0");
+			
+			bw5.write("1100");
+			bw5.write(" 0");
+			
+			bw1.close();
+			bw2.close();
+			bw3.close();
+			bw4.close();
+			bw5.close();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
-		return 0;
+
 	}
 	
 	public static void main(String[] args) {
